79b0bddb065803c3ecee05186d3454e509f569b1
Aliaksandr Liakh <liakh.aliaksandr@gmail.com>
2022-06-08 17:47:50 +0300

    Update

---
+++ src/main/java/part2/part1/NumbersPublisher.java
@@ -0,0 +1,33 @@
+package part2.part1;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.SubmissionPublisher;
+import java.util.stream.IntStream;
+
+public class NumbersPublisher extends SubmissionPublisher<Integer> {
+
+    private static final Logger logger = LoggerFactory.getLogger(NumbersPublisher.class);
+
+    private final int count;
+
+    NumbersPublisher(int count) {
+        this.count = count;
+    }
+
+    public static void main(String[] args) {
+        NumbersPublisher publisher = new NumbersPublisher(10);
+
+        NumbersSubscriber subscriber = new NumbersSubscriber();
+        publisher.subscribe(subscriber);
+
+        IntStream.range(0, publisher.count)
+            .forEach(i -> {
+                logger.info("Publish: {}", i);
+                publisher.submit(i);
+            });
+
+        publisher.close();
+    }
+}
---
+++ src/main/java/part2/part1/NumbersSubscriber.java
@@ -0,0 +1,37 @@
+package part2.part1;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.Flow;
+
+public class NumbersSubscriber implements Flow.Subscriber<Integer> {
+
+    private static final Logger logger = LoggerFactory.getLogger(NumbersSubscriber.class);
+
+    private Flow.Subscription subscription;
+
+    @Override
+    public void onSubscribe(Flow.Subscription subscription) {
+        logger.info("Subscriber.onSubscribe: {}", subscription);
+        this.subscription = subscription;
+        this.subscription.request(1);
+    }
+
+    @Override
+    public void onNext(Integer item) {
+        logger.info("Subscriber.onNext: {}", item);
+        this.subscription.request(1);
+    }
+
+    @Override
+    public void onError(Throwable throwable) {
+        logger.info("Subscriber.onError: {}", throwable.getMessage());
+        throwable.printStackTrace();
+    }
+
+    @Override
+    public void onComplete() {
+        logger.info("Subscriber.onComplete");
+    }
+}
