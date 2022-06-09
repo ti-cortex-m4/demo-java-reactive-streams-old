f6eeae2c3a64e6802b695d0bd7c2f3fe393187f1
Aliaksandr Liakh <liakh.aliaksandr@gmail.com>
2022-06-09 12:04:19 +0300

    Update

--- src/main/java/part0/SimplePublisher.java
+++ src/main/java/part0/SimplePublisher.java
@@ -21,27 +21,7 @@ public class SimplePublisher implements Flow.Publisher<Integer> {
     }
 
     public static void main(String[] args) {
-        new SimplePublisher(10).subscribe(new Flow.Subscriber<>() {
-            @Override
-            public void onSubscribe(Flow.Subscription subscription) {
-                System.out.println("onSubscribe " + subscription);
-            }
-
-            @Override
-            public void onNext(Integer item) {
-                System.out.println("item = [" + item + "]");
-            }
-
-            @Override
-            public void onError(Throwable throwable) {
-                System.out.println("throwable = [" + throwable + "]");
-            }
-
-            @Override
-            public void onComplete() {
-                System.out.println("complete");
-            }
-        });
+        new SimplePublisher(10).subscribe(new SimpleSubscriber());
     }
 
     private class SimpleSubscription implements Flow.Subscription {
---
+++ src/main/java/part0/SimpleSubscriber.java
@@ -0,0 +1,29 @@
+package part0;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.Flow;
+
+public class SimpleSubscriber implements Flow.Subscriber<Integer> {
+
+    @Override
+    public void onSubscribe(Flow.Subscription subscription) {
+        System.out.println("onSubscribe " + subscription);
+    }
+
+    @Override
+    public void onNext(Integer item) {
+        System.out.println("item = [" + item + "]");
+    }
+
+    @Override
+    public void onError(Throwable throwable) {
+        System.out.println("throwable = [" + throwable + "]");
+    }
+
+    @Override
+    public void onComplete() {
+        System.out.println("complete");
+    }
+}
