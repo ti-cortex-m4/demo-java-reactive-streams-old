7d5794bd9144a17bf46e7e3ba9f83e3ec45c3d50
Aliaksandr Liakh <liakh.aliaksandr@gmail.com>
2022-07-10 21:59:53 +0300

    Update

--- src/main/java/part3/HttpClientExample.java
+++ src/main/java/part3/HttpClientExample.java
@@ -11,17 +11,24 @@ import java.net.http.HttpRequest.BodyPublishers;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
 import java.net.http.HttpResponse.PushPromiseHandler;
+import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Flow;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class HttpClientExample {
 
     public static void main(String[] args) throws Exception {
-        //httpPostRequest();
+        httpPostRequest1();
 
+
+    }
+
+    public static void httpPostRequest1() throws URISyntaxException, IOException, InterruptedException {
         HttpClient client = HttpClient.newBuilder()
             .version(HttpClient.Version.HTTP_2)
             .build();
@@ -37,17 +44,61 @@ public class HttpClientExample {
         System.out.println("httpPostRequest : " + responseBody);
     }
 
-    public static void httpPostRequest() throws URISyntaxException, IOException, InterruptedException {
+    public static void httpPostRequest2() throws URISyntaxException, IOException, InterruptedException {
         HttpClient client = HttpClient.newBuilder()
             .version(HttpClient.Version.HTTP_2)
             .build();
-        HttpRequest request = HttpRequest.newBuilder(new URI("http://jsonplaceholder.typicode.com/posts"))
-            .version(HttpClient.Version.HTTP_2)
-            .POST(BodyPublishers.ofString("Sample Post Request"))
+
+        Flow.Publisher<? extends ByteBuffer> publisher = new Flow.Publisher<ByteBuffer>() {
+            @Override
+            public void subscribe(Flow.Subscriber<? super ByteBuffer> subscriber) {
+                subscriber.onSubscribe(new Flow.Subscription() {
+                    @Override
+                    public void request(long n) {
+                    }
+
+                    @Override
+                    public void cancel() {
+                    }
+                });
+                subscriber.onNext(ByteBuffer.wrap("hello".getBytes(Charset.defaultCharset())));
+                subscriber.onComplete();
+            }
+        };
+
+        HttpRequest request = HttpRequest.newBuilder()
+            .uri(new URI("https://postman-echo.com/post"))
+            .headers("Content-Type", "text/plain;charset=UTF-8")
+            .POST(HttpRequest.BodyPublishers.fromPublisher(publisher))
             .build();
-        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
-        String responseBody = response.body();
-        System.out.println("httpPostRequest : " + responseBody);
+
+        Flow.Subscriber<? super String> subscriber = new Flow.Subscriber<String>() {
+            
+            private Flow.Subscription subscription;
+            
+            @Override
+            public void onSubscribe(Flow.Subscription subscription) {
+                this.subscription = subscription;
+            }
+
+            @Override
+            public void onNext(String item) {
+                System.out.println("Subscriber.onNext: {}"+ item);
+                this.subscription.request(1);
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+                System.out.println("Subscriber.onError: {}"+ throwable.getMessage());
+                throwable.printStackTrace();
+            }
+
+            @Override
+            public void onComplete() {
+                System.out.println("Subscriber.onComplete");
+            }
+        };
+        HttpResponse<Void> response = client.send(request, BodyHandlers.fromLineSubscriber(subscriber));
     }
 
 }
