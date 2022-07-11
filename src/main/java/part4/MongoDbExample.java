50695b99c014bacbd16a2387c33a0654ebf1d8c0
Aliaksandr Liakh <liakh.aliaksandr@gmail.com>
2022-07-11 19:04:10 +0300

    Update

---
+++ src/main/java/part4/MongoDbExample.java
@@ -0,0 +1,56 @@
+package part4;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import de.flapdoodle.embed.mongo.MongodExecutable;
+import de.flapdoodle.embed.mongo.MongodProcess;
+import de.flapdoodle.embed.mongo.MongodStarter;
+import de.flapdoodle.embed.mongo.config.MongodConfig;
+import de.flapdoodle.embed.mongo.config.Net;
+import de.flapdoodle.embed.mongo.distribution.Version;
+import de.flapdoodle.embed.process.runtime.Network;
+import org.bson.Document;
+import org.bson.types.ObjectId;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
+import java.util.List;
+import java.util.concurrent.Flow;
+
+public class MongoDbExample {
+
+    public static void main(String[] args) throws Exception {
+        MongodStarter starter = MongodStarter.getDefaultInstance();
+
+        int port = Network.getFreeServerPort();
+        MongodConfig mongodConfig = MongodConfig.builder()
+            .version(Version.Main.PRODUCTION)
+            .net(new Net(port, Network.localhostIsIPv6()))
+            .build();
+
+        MongodExecutable mongodExecutable = null;
+        try {
+            mongodExecutable = starter.prepare(mongodConfig);
+            MongodProcess mongod = mongodExecutable.start();
+
+            try (MongoClient mongoClient = new MongoClient("localhost", port)) {
+                MongoDatabase database = mongoClient.getDatabase("test");
+                Document document = database.runCommand(new Document("buildInfo", 1));
+                System.out.println(document.getString("version"));
+            }
+
+        } finally {
+            if (mongodExecutable != null)
+                mongodExecutable.stop();
+        }
+    }
+}
---
+++ src/main/java/part4/ReactiveMongoDbExample.java
@@ -0,0 +1,41 @@
+package part4;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
+import de.flapdoodle.embed.mongo.MongodExecutable;
+import de.flapdoodle.embed.mongo.MongodProcess;
+import de.flapdoodle.embed.mongo.MongodStarter;
+import de.flapdoodle.embed.mongo.config.MongodConfig;
+import de.flapdoodle.embed.mongo.config.Net;
+import de.flapdoodle.embed.mongo.distribution.Version;
+import de.flapdoodle.embed.process.runtime.Network;
+import org.bson.Document;
+
+public class ReactiveMongoDbExample {
+
+    public static void main(String[] args) throws Exception {
+        MongodStarter starter = MongodStarter.getDefaultInstance();
+
+        int port = Network.getFreeServerPort();
+        MongodConfig mongodConfig = MongodConfig.builder()
+            .version(Version.Main.PRODUCTION)
+            .net(new Net(port, Network.localhostIsIPv6()))
+            .build();
+
+        MongodExecutable mongodExecutable = null;
+        try {
+            mongodExecutable = starter.prepare(mongodConfig);
+            MongodProcess mongod = mongodExecutable.start();
+
+            try (MongoClient mongoClient = new MongoClient("localhost", port)) {
+                MongoDatabase database = mongoClient.getDatabase("test");
+                Document document = database.runCommand(new Document("buildInfo", 1));
+                System.out.println(document.getString("version"));
+            }
+
+        } finally {
+            if (mongodExecutable != null)
+                mongodExecutable.stop();
+        }
+    }
+}
