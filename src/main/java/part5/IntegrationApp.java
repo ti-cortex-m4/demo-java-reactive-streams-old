5b317ce4c0678735af1769194de4d878560cdc5b
Aliaksandr Liakh <liakh.aliaksandr@gmail.com>
2022-07-12 16:22:12 +0300

    Update

--- src/main/java/part5/IntegrationApp.java
+++ src/main/java/part5/IntegrationApp.java
@@ -1,34 +1,37 @@
 package part5;
 
+import akka.NotUsed;
+import akka.actor.ActorSystem;
+import akka.stream.ActorMaterializer;
+import akka.stream.javadsl.Flow;
+import akka.stream.javadsl.JavaFlowSupport;
+import io.reactivex.Flowable;
+import org.reactivestreams.FlowAdapters;
 import reactor.adapter.JdkFlowAdapter;
 import reactor.core.publisher.Flux;
 
 import java.time.Duration;
-
+import java.util.concurrent.Flow.Processor;
 import java.util.concurrent.Flow.Publisher;
-import java.util.concurrent.Flow.Subscriber;
-import org.reactivestreams.Subscription;
-import reactor.core.CoreSubscriber;
-import reactor.core.publisher.Flux;
 
 public class IntegrationApp {
 
     public static void main(String[] args) {
         Publisher<Long> reactorPublisher = reactorPublisher();
 
-//        Processor<Long, Long> akkaStreamsProcessor = akkaStreamsProcessor();
-//        reactorPublisher.subscribe(akkaStreamsProcessor);
-//
-//        Flowable
-//            .fromPublisher(FlowAdapters.toProcessor(akkaStreamsProcessor))
-//            .subscribe(System.out::println);
+        Processor<Long, Long> akkaStreamsProcessor = akkaStreamsProcessor();
+        reactorPublisher.subscribe(akkaStreamsProcessor);
+
+        Flowable
+            .fromPublisher(FlowAdapters.toProcessor(akkaStreamsProcessor))
+            .subscribe(System.out::println);
     }
 
     private static Publisher<Long> reactorPublisher() {
         Flux<Long> numberFlux = Flux.interval(Duration.ofSeconds(1));
         return JdkFlowAdapter.publisherToFlowPublisher(numberFlux);
     }
-/*
+
     private static Processor<Long, Long> akkaStreamsProcessor() {
         Flow<Long, Long, NotUsed> negatingFlow = Flow.of(Long.class).map(i -> -i);
         return JavaFlowSupport.Flow.toProcessor(negatingFlow).run(materializer);
@@ -36,5 +39,5 @@ public class IntegrationApp {
 
     private static ActorSystem actorSystem = ActorSystem.create();
     private static ActorMaterializer materializer = ActorMaterializer.create(actorSystem);
- */
+
 }
