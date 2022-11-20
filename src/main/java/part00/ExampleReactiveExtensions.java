9524ee85f09c94ab935edfe0c4453116ee710866
Aliaksandr Liakh <liakh.aliaksandr@gmail.com>
2022-11-20 17:37:39 +0300

    up

--- src/main/java/part00/ExampleReactiveExtensions.java
+++ src/main/java/part00/ExampleReactiveExtensions.java
@@ -1,34 +1,34 @@
 package part00;
 
-import io.reactivex.Observable;
-import io.reactivex.Observer;
-import io.reactivex.disposables.Disposable;
+import rx.Observable;
+import rx.Observer;
+import rx.Subscription;
 
 public class ExampleReactiveExtensions {
-/*
-    private final Observable observable = new Observable();
 
-    public void example() {
-        observable.subscribe(new Observer() {
+    public static void main(String[] args) {
+        Observer<String> observer = new Observer<>() {
             @Override
-            public void onSubscribe(Disposable d) {
+            public void onCompleted() {
+                System.out.println("completed");
             }
 
             @Override
-            public void onNext(Object o) {
+            public void onError(Throwable throwable) {
+                System.out.println("error: " + throwable);
             }
 
             @Override
-            public void onError(Throwable e) {
+            public void onNext(String message) {
+                System.out.println("received: " + message);
             }
+        };
+        Observable<String> observable = Observable.just("a", "b", "c");
+        Subscription subscription = observable
+            .filter(s -> !s.isEmpty())
+            .map(s -> s.toUpperCase())
+            .subscribe(observer);
 
-            @Override
-            public void onComplete() {
-            }
-        });
+        subscription.unsubscribe();
     }
-
-    private static class Value {
-    }
- */
 }
