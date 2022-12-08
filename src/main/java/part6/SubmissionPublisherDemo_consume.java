b7264ef004f1921edf51210aa0ea49c8ddf35aa4
Aliaksandr Liakh <liakh.aliaksandr@gmail.com>
2022-12-08 21:06:56 +0300

    Update

--- src/main/java/part6/SubmissionPublisherDemo_consume.java
+++ src/main/java/part6/SubmissionPublisherDemo_consume.java
@@ -10,7 +10,9 @@ public class SubmissionPublisherDemo_consume {
     public static void main(String[] args) throws InterruptedException, ExecutionException {
         try (SubmissionPublisher<Long> publisher = new SubmissionPublisher<>()) {
             CompletableFuture<Void> future = publisher.consume(System.out::println);
+
             LongStream.range(0, 10).forEach(publisher::submit);
+
             future.get();
         }
     }
--- src/main/java/part6/SubmissionPublisherDemo_subscribe.java
+++ src/main/java/part6/SubmissionPublisherDemo_subscribe.java
@@ -2,25 +2,39 @@ package part6;
 
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Flow;
 import java.util.concurrent.SubmissionPublisher;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.LongStream;
 
 public class SubmissionPublisherDemo_subscribe {
 
     public static void main(String[] args) throws InterruptedException, ExecutionException {
         try (SubmissionPublisher<Long> publisher = new SubmissionPublisher<>()) {
+            System.out.println("getExecutor: " + publisher.getExecutor());
+            System.out.println("getMaxBufferCapacity: " + publisher.getMaxBufferCapacity());
+
             publisher.subscribe(new Flow.Subscriber<>() {
+
+                private Flow.Subscription subscription;
 
                 @Override
                 public void onSubscribe(Flow.Subscription subscription) {
-                    subscription.request(Long.MAX_VALUE);
+                    this.subscription = subscription;
+
+                    this.subscription.request(2);
                     System.out.println("subscribed: " + subscription);
                 }
 
                 @Override
                 public void onNext(Long item) {
+                    this.subscription.request(2);
+
                     System.out.println("next: " + item);
+                    System.out.println("estimateMaximumLag: " + publisher.estimateMaximumLag());
+                    System.out.println("estimateMinimumDemand: " + publisher.estimateMinimumDemand());
                 }
 
                 @Override
@@ -33,7 +47,12 @@ public class SubmissionPublisherDemo_subscribe {
                     System.out.println("completed");
                 }
             });
+            System.out.println("getNumberOfSubscribers: " + publisher.getNumberOfSubscribers());
+
             LongStream.range(0, 10).forEach(publisher::submit);
+
+            ExecutorService executor = (ExecutorService)publisher.getExecutor();
+            executor.awaitTermination(1, TimeUnit.SECONDS);
         }
     }
 }
