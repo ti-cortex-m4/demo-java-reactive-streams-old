28dd515b0f67c6eef3e3c5d39ebe1b8ce1f0ba83
Aliaksandr Liakh <liakh.aliaksandr@gmail.com>
2023-03-21 10:18:26 +0300

    update

--- chapter-03/rxjava-reactivestreams-ratpack/src/main/java/org/rpis5/chapters/chapter_03/rxjava_reactivestreams/FileService.java
+++
@@ -1,8 +0,0 @@
-package org.rpis5.chapters.chapter_03.rxjava_reactivestreams;
-
-import org.reactivestreams.Publisher;
-
-public interface FileService {
-
-    void writeTo(String file, Publisher<String> content);
-}
