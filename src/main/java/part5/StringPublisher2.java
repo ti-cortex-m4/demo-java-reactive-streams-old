21bbeb619bdb3295aa914fc05208b6c4410dff50
Aliaksandr Liakh <liakh.aliaksandr@gmail.com>
2023-03-22 17:42:06 +0300

    update

--- src/main/java/part5/StringPublisher2.java
+++ src/main/java/part5/StringPublisher2.java
@@ -3,27 +3,25 @@ package part5;
 import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
 import java.util.concurrent.Flow;
+import java.util.concurrent.SubmissionPublisher;
 
-public class StringPublisher2 implements Flow.Publisher<ByteBuffer>{
+public class StringPublisher2 extends SubmissionPublisher<ByteBuffer> {
 
     private final String string;
+    private Flow.Subscription subscription;
 
     public StringPublisher2(String string) {
         this.string = string;
     }
 
-    @Override
-    public void subscribe(Flow.Subscriber<? super ByteBuffer> subscriber) {
-        subscriber.onSubscribe(new Flow.Subscription() {
-            @Override
-            public void request(long n) {
-            }
-
-            @Override
-            public void cancel() {
-            }
-        });
-        subscriber.onNext(ByteBuffer.wrap(string.getBytes(StandardCharsets.UTF_8)));
-        subscriber.onComplete();
+    public void onSubscribe(Flow.Subscription subscription) {
+        (this.subscription = subscription).request(1);
     }
+
+    public void onNext(ByteBuffer item) {
+        subscription.request(1);
+    }
+
+    public void onError(Throwable ex) { closeExceptionally(ex); }
+    public void onComplete() { close(); }
 }
