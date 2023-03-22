04df207572823b3ae3afd41090293ab952fb8e9c
Aliaksandr Liakh <liakh.aliaksandr@gmail.com>
2023-03-22 17:38:41 +0300

    update

---
+++ src/main/java/part5/StringPublisher2.java
@@ -0,0 +1,29 @@
+package part5;
+
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.Flow;
+
+public class StringPublisher2 implements Flow.Publisher<ByteBuffer>{
+
+    private final String string;
+
+    public StringPublisher2(String string) {
+        this.string = string;
+    }
+
+    @Override
+    public void subscribe(Flow.Subscriber<? super ByteBuffer> subscriber) {
+        subscriber.onSubscribe(new Flow.Subscription() {
+            @Override
+            public void request(long n) {
+            }
+
+            @Override
+            public void cancel() {
+            }
+        });
+        subscriber.onNext(ByteBuffer.wrap(string.getBytes(StandardCharsets.UTF_8)));
+        subscriber.onComplete();
+    }
+}
