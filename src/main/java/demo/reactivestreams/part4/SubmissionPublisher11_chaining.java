4243c7829e1aae78ae8914d44ff9e7a3090eaf0d
Aliaksandr Liakh <liakh.aliaksandr@gmail.com>
2023-04-02 19:11:08 +0300

    Update

--- src/main/java/demo/reactivestreams/part4/SubmissionPublisher11_chaining.java
+++ src/main/java/demo/reactivestreams/part4/SubmissionPublisher11_chaining.java
@@ -1,5 +1,6 @@
 package demo.reactivestreams.part4;
 
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.SubmissionPublisher;
@@ -12,18 +13,18 @@ public class SubmissionPublisher11_chaining extends SomeTest {
              SubmissionPublisher<Integer> publisher2 = new SubmissionPublisher<>();
              SubmissionPublisher<Integer> publisher3 = new SubmissionPublisher<>()) {
 
-            publisher3.consume(item -> {
+            CompletableFuture<Void> consumerFuture3 =publisher3.consume(item -> {
                 delay();
                 logger.info("step 3: {}", item);
             });
 
-            publisher2.consume(item -> {
+            CompletableFuture<Void> consumerFuture2 =publisher2.consume(item -> {
                 delay();
                 logger.info("step 2: {}", item);
                 publisher3.submit(item * item);
             });
 
-            publisher1.consume(item -> {
+            CompletableFuture<Void> consumerFuture1 =publisher1.consume(item -> {
                 delay();
                 logger.info("step 1: {}", item);
                 publisher2.submit(item * item);
@@ -33,8 +34,15 @@ public class SubmissionPublisher11_chaining extends SomeTest {
             publisher1.submit(3);
             publisher1.submit(5);
             publisher1.submit(7);
+            publisher1.close();
 
-            ForkJoinPool.commonPool().awaitTermination(60, TimeUnit.SECONDS);
+            CompletableFuture<Void> futures = CompletableFuture.allOf(consumerFuture1,consumerFuture2,consumerFuture3);
+            logger.info("wait...");
+
+            futures.get();
+            logger.info("completed");
+
+//            ForkJoinPool.commonPool().awaitTermination(60, TimeUnit.SECONDS);
         }
     }
 }
