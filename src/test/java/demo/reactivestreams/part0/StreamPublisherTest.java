ac07e518f4ce47483e1a1264797f16efe3d9a966
Aliaksandr Liakh <liakh.aliaksandr@gmail.com>
2023-04-04 21:54:14 +0300

    Update

--- src/test/java/demo/reactivestreams/part0/StreamPublisherTest.java
+++ src/test/java/demo/reactivestreams/part0/StreamPublisherTest.java
@@ -1,59 +1,27 @@
 package demo.reactivestreams.part0;
 
-import org.reactivestreams.Publisher;
-import org.reactivestreams.tck.PublisherVerification;
 import org.reactivestreams.tck.TestEnvironment;
+import org.reactivestreams.tck.flow.FlowPublisherVerification;
 import org.testng.annotations.Test;
 
-import java.lang.reflect.Field;
+import java.util.concurrent.Flow;
 import java.util.function.UnaryOperator;
 import java.util.stream.Stream;
 
-public class StreamPublisherTest extends PublisherVerification<Integer> {
-    private final TestEnvironment env;
+@Test
+public class StreamPublisherTest extends FlowPublisherVerification<Integer> {
 
-    public StreamPublisherTest() throws NoSuchFieldException, IllegalAccessException {
-        super(new TestEnvironment(400));
-
-        Field env = PublisherVerification.class.getDeclaredField("env");
-        env.setAccessible(true);
-        this.env = (TestEnvironment) env.get(this);
+    public StreamPublisherTest() {
+        super(new TestEnvironment());
     }
 
     @Override
-    public Publisher<Integer> createPublisher(long elements) {
-        return ReactiveStreamsFlowBridge.toReactiveStreams(
-                new StreamPublisher<>(() -> Stream.iterate(0, UnaryOperator.identity()).limit(elements))
-        );
+    public Flow.Publisher<Integer> createFlowPublisher(long elements) {
+        return new StreamPublisher<>(() -> Stream.iterate(0, UnaryOperator.identity()).limit(elements));
     }
 
     @Override
-    public Publisher<Integer> createFailedPublisher() {
-        return ReactiveStreamsFlowBridge.toReactiveStreams(
-                new StreamPublisher<Integer>(() -> {
-                    throw new RuntimeException();
-                })
-        );
-    }
-
-    @Test
-    public void required_spec317_mustSupportACumulativePendingElementCountGreaterThenLongMaxValue() throws Throwable {
-        final int totalElements = 50;
-
-        activePublisherTest(totalElements, true, pub -> {
-            final TestEnvironment.ManualSubscriber<Integer> sub = env.newManualSubscriber(pub);
-            new Thread(() -> sub.request(Long.MAX_VALUE)).start();
-            new Thread(() -> sub.request(Long.MAX_VALUE)).start();
-
-            sub.nextElements(totalElements);
-            sub.expectCompletion();
-
-            try {
-                env.verifyNoAsyncErrorsNoDelay();
-            } finally {
-                sub.cancel();
-            }
-
-        });
+    public Flow.Publisher<Integer> createFailedFlowPublisher() {
+        return new StreamPublisher<Integer>(() -> {            throw new RuntimeException();        });
     }
 }
