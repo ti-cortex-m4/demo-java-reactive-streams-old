37a5cb3f25a285200a5730a9c3a728c4c0946aec
Aliaksandr Liakh <liakh.aliaksandr@gmail.com>
2023-04-05 10:17:13 +0300

    Update

--- src/main/java/demo/reactivestreams/part1/SimpleSubscriber.java
+++ src/main/java/demo/reactivestreams/part1/SimpleSubscriber.java
@@ -1,26 +1,31 @@
 package demo.reactivestreams.part1;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.concurrent.Flow;
 
 public class SimpleSubscriber implements Flow.Subscriber<Integer> {
 
+    private static final Logger logger = LoggerFactory.getLogger(SimpleSubscriber.class);
+
     @Override
     public void onSubscribe(Flow.Subscription subscription) {
-        System.out.println("onSubscribe: " + subscription);
+        logger.info("subscribed: {}", subscription);
     }
 
     @Override
     public void onNext(Integer item) {
-        System.out.println("onNext: " + item);
+        logger.info("next: {}", item);
     }
 
     @Override
     public void onError(Throwable throwable) {
-        System.out.println("onError " + throwable);
+        logger.error("error", throwable);
     }
 
     @Override
     public void onComplete() {
-        System.out.println("onComplete");
+        logger.info("completed");
     }
 }
