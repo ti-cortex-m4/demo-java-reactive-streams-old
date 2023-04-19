fb3d62c4cc34173747f969bddaf34aa59b3b9ffc
Aliaksandr Liakh <liakh.aliaksandr@gmail.com>
2023-04-19 22:17:02 +0300

    Update

--- src/main/java/demo/reactivestreams/part1a/AbstractSyncSubscriber.java
+++ src/main/java/demo/reactivestreams/part1a/AbstractSyncSubscriber.java
@@ -5,13 +5,6 @@ import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.Flow;
 
-/**
- * SyncSubscriber is an implementation of Reactive Streams `Subscriber`,
- * it runs synchronously (on the Publisher's thread) and requests one element
- * at a time and invokes a user-defined method to process each element.
- * <p>
- * NOTE: The code below uses a lot of try-catches to show the reader where exceptions can be expected, and where they are forbidden.
- */
 public abstract class AbstractSyncSubscriber<T> implements Flow.Subscriber<T> {
 
     private static final Logger logger = LoggerFactory.getLogger(AbstractSyncSubscriber.class);
@@ -23,17 +16,14 @@ public abstract class AbstractSyncSubscriber<T> implements Flow.Subscriber<T> {
     public void onSubscribe(Flow.Subscription s) {
         logger.info("subscriber.subscribe: {}", s);
         if (s == null) {
-          throw new NullPointerException();
+            throw new NullPointerException();
         }
 
-        if (this.subscription != null) { // If someone has made a mistake and added this Subscriber multiple times, let's handle it gracefully
-            s.cancel(); // Cancel the additional subscription
+        if (this.subscription != null) {
+            s.cancel();
         } else {
-            // We have to assign it locally before we use it, if we want to be a synchronous `Subscriber`
-            // Because according to rule 3.10, the Subscription is allowed to call `onNext` synchronously from within `request`
             this.subscription = s;
-
-            s.request(1); // Our Subscriber is unbuffered and modest, it requests one element at a time
+            s.request(1);
         }
     }
 
@@ -41,36 +31,26 @@ public abstract class AbstractSyncSubscriber<T> implements Flow.Subscriber<T> {
     public void onNext(T element) {
         logger.info("subscriber.next: {}", element);
         if (element == null) {
-          throw new NullPointerException();
+            throw new NullPointerException();
         }
 
         if (!done) {
-                if (whenNext(element)) {
-                    this.subscription.request(1); // Our Subscriber is unbuffered and modest, it requests one element at a time
-                } else {
-                    done();
-                }
+            if (whenNext(element)) {
+                this.subscription.request(1);
+            } else {
+                done = true;
+                this.subscription.cancel();
+            }
         }
     }
 
-    // Showcases a convenience method to idempotently marking the Subscriber as "done", so we don't want to process more elements
-    // herefor we also need to cancel our `Subscription`.
-    private void done() {
-        //On this line we could add a guard against `!done`, but since rule 3.7 says that `Subscription.cancel()` is idempotent, we don't need to.
-        done = true; // If we `whenNext` throws an exception, let's consider ourselves done (not accepting more elements)
-
-        this.subscription.cancel(); // Cancel the subscription
-    }
-
-    // This method is left as an exercise to the reader/extension point
-    // Returns whether more elements are desired or not, and if no more elements are desired
-    protected abstract boolean whenNext(final T element);
+    protected abstract boolean whenNext(T element);
 
     @Override
     public void onError(Throwable throwable) {
         logger.error("subscriber.error", throwable);
         if (throwable == null) {
-          throw new NullPointerException();
+            throw new NullPointerException();
         }
     }
 
