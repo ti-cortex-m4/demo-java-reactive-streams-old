2d1513b7312b7445e5607c58862ea62471880913
Aliaksandr Liakh <liakh.aliaksandr@gmail.com>
2023-04-21 19:16:42 +0300

    Update

--- src/main/java/demo/reactivestreams/part1c/Runner.java
+++ src/main/java/demo/reactivestreams/part1c/Runner.java
@@ -5,13 +5,14 @@ import demo.reactivestreams.part1.TckCompatibleIteratorPublisher;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class Runner {
 
     public static void main(String[] args) throws InterruptedException {
         List<Integer> list = List.of(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
         ExecutorService executorService = Executors.newFixedThreadPool(2);
-        NumberIterablePublisher publisher = new NumberIterablePublisher(0,9,executorService);
+        AsyncIterablePublisher<Integer> publisher = new AsyncIterablePublisher<>(() -> list.iterator(),executorService);
 
         SyncSubscriber<Integer> subscriber1 = new SyncSubscriber<>(1);
         publisher.subscribe(subscriber1);
@@ -21,5 +22,8 @@ public class Runner {
 
         subscriber1.awaitCompletion();
 //        subscriber2.awaitCompletion();
+
+        executorService.shutdown();
+        executorService.awaitTermination(60, TimeUnit.SECONDS);
     }
 }
