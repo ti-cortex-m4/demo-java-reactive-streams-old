ba98b7a831bfe6560f4110e33002091618602b04
Aliaksandr Liakh <liakh.aliaksandr@gmail.com>
2023-04-21 19:06:21 +0300

    Update

---
+++ src/test/java/demo/reactivestreams/part1c/IterablePublisherTest.java
@@ -0,0 +1,44 @@
+package demo.reactivestreams.part1c;
+
+import org.reactivestreams.Publisher;
+import org.reactivestreams.example.unicast.publisher.AsyncIterablePublisher;
+import org.reactivestreams.example.unicast.publisher.NumberIterablePublisher;
+import org.reactivestreams.tck.PublisherVerification;
+import org.reactivestreams.tck.TestEnvironment;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.Iterator;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@Test // Must be here for TestNG to find and run this, do not remove
+public class IterablePublisherTest extends PublisherVerification<Integer> {
+
+  private ExecutorService e;
+  @BeforeClass void before() { e = Executors.newFixedThreadPool(4); }
+  @AfterClass void after() { if (e != null) e.shutdown(); }
+
+  public IterablePublisherTest() {
+    super(new TestEnvironment());
+  }
+
+  @SuppressWarnings("unchecked")
+  @Override public Publisher<Integer> createPublisher(final long elements) {
+    assert(elements <= maxElementsFromPublisher());
+    return new NumberIterablePublisher(0, (int)elements, e);
+  }
+
+  @Override public Publisher<Integer> createFailedPublisher() {
+    return new AsyncIterablePublisher<Integer>(new Iterable<Integer>() {
+      @Override public Iterator<Integer> iterator() {
+        throw new RuntimeException("Error state signal!");
+      }
+    }, e);
+  }
+
+  @Override public long maxElementsFromPublisher() {
+    return Integer.MAX_VALUE;
+  }
+}
