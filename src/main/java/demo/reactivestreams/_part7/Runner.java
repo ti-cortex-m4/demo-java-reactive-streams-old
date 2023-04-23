2dda0f3df3840e5a951d38a452f9923ed1fd4b1d
Aliaksandr Liakh <liakh.aliaksandr@gmail.com>
2023-04-23 21:12:29 +0300

    Update

--- src/main/java/demo/reactivestreams/_part7/Runner.java
+++ src/main/java/demo/reactivestreams/_part7/Runner.java
@@ -1,37 +1,29 @@
 package demo.reactivestreams._part7;
 
+import demo.reactivestreams._part6.SyncSubscriber;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Iterator;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.SubmissionPublisher;
 import java.util.stream.IntStream;
 
-public class SubmissionIteratorPublisher extends SubmissionPublisher<Integer> {
+public class Runner {
 
-    private static final Logger logger = LoggerFactory.getLogger(SubmissionIteratorPublisher.class);
-
-    private final int count;
-
-    public SubmissionIteratorPublisher(int count) {
-        this.count = count;
-    }
-
-    public int getCount() {
-        return count;
-    }
+    private static final Logger logger = LoggerFactory.getLogger(Runner.class);
 
     public static void main(String[] args) throws InterruptedException {
         CountDownLatch countDownLatch = new CountDownLatch(1);
 
-        SubmissionIteratorPublisher publisher = new SubmissionIteratorPublisher(10);
+        SubmissionPublisher<Integer> publisher = new SubmissionPublisher<>();
         SubmissionProcessor processor = new SubmissionProcessor();
         BackpressureSubscriber subscriber = new BackpressureSubscriber(countDownLatch);
 
         processor.subscribe(subscriber);
         publisher.subscribe(processor);
 
-        IntStream.range(0, publisher.getCount()).forEach(i -> {
+        IntStream.range(0, 10).forEach(i -> {
             logger.info("publisher.submit: {}", i);
             publisher.submit(i);
         });
