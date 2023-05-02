5a0dd9bc5c4a06e40a4d4ad0a1225f97b607c4a0
Aliaksandr Liakh <liakh.aliaksandr@gmail.com>
2023-05-02 21:03:07 +0300

    Update

--- src/main/java/demo/reactivestreams/part4/RunnerSubmissionPublisher.java
+++ src/main/java/demo/reactivestreams/part4/RunnerSubmissionPublisher.java
@@ -1,26 +1,27 @@
 package demo.reactivestreams.part4;
 
+import demo.reactivestreams.part1.SyncSubscriber;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Iterator;
+import java.util.List;
 import java.util.concurrent.SubmissionPublisher;
-import java.util.stream.IntStream;
 
 public class RunnerSubmissionPublisher {
 
     private static final Logger logger = LoggerFactory.getLogger(RunnerSubmissionPublisher.class);
 
     public static void main(String[] args) throws InterruptedException {
-        SubmissionPublisher<Integer> publisher = new SubmissionPublisher<>();
+        SubmissionPublisher<String> publisher = new SubmissionPublisher<>();
 
-        SyncSubscriber<Integer> subscriber1 = new SyncSubscriber<>(1);
+        SyncSubscriber<String> subscriber1 = new SyncSubscriber<>(1);
         publisher.subscribe(subscriber1);
 
-        SyncSubscriber<Integer> subscriber2 = new SyncSubscriber<>(2);
+        SyncSubscriber<String> subscriber2 = new SyncSubscriber<>(2);
         publisher.subscribe(subscriber2);
 
-        Iterator<Integer> iterator = IntStream.rangeClosed(0, 9).iterator();
+        Iterator<String> iterator = List.of("The quick brown fox jumps over the lazy dog.".split(" ")).iterator();
         iterator.forEachRemaining(item -> {
             logger.info("publisher.submit: {}", item);
             publisher.submit(item);
