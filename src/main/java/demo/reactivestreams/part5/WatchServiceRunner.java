02fe45dd2c563a38360536db37167899e9d73b30
Aliaksandr Liakh <liakh.aliaksandr@gmail.com>
2023-05-05 17:20:24 +0300

    Update

--- src/main/java/demo/reactivestreams/part5/WatchServiceRunner.java
+++ src/main/java/demo/reactivestreams/part5/WatchServiceRunner.java
@@ -1,16 +1,11 @@
 package demo.reactivestreams.part5;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.nio.file.Path;
 import java.nio.file.WatchEvent;
 import java.util.concurrent.SubmissionPublisher;
 import java.util.concurrent.TimeUnit;
 
 public class WatchServiceRunner {
-
-    private static final Logger logger = LoggerFactory.getLogger(WatchServiceRunner.class);
 
     public static void main(String[] args) throws InterruptedException {
         String folderName = System.getProperty("user.home");
@@ -25,7 +20,6 @@ public class WatchServiceRunner {
 
             TimeUnit.SECONDS.sleep(180);
 
-            logger.info("runner.close");
             publisher.close();
 
             subscriber.awaitCompletion();
