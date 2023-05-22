34e4eb108dbfa6bffbabdac0753860af017f3841
Aliaksandr Liakh <liakh.aliaksandr@gmail.com>
2023-05-22 17:37:53 +0300

    Update

---
+++ src/main/java/demo/reactivestreams/Runner.java
@@ -0,0 +1,33 @@
+package demo.reactivestreams;
+
+import demo.reactivestreams.part1.SyncPublisherSyncSubscriberRunner;
+import demo.reactivestreams.part2.AsyncPublisherAsyncSubscriberRunner;
+import demo.reactivestreams.part5.FileRunner;
+import demo.reactivestreams.part5.WatchServiceRunner;
+
+import java.io.IOException;
+import java.util.concurrent.Executors;
+
+public class Runner {
+
+    public static void main(String[] args) throws InterruptedException, IOException {
+        SyncPublisherSyncSubscriberRunner.main(new String[] {});
+        AsyncPublisherAsyncSubscriberRunner.main(new String[] {});
+
+        WatchServiceRunner.main(new String[] {});
+
+        Thread thread =  new Thread() {
+            @Override
+            public void run(){
+                try {
+                    FileRunner.main(new String[]{});
+                } catch (InterruptedException | IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        };
+
+        thread.start();
+        thread.join();
+    }
+}
---
+++ src/main/java/demo/reactivestreams/part5/FileRunner.java
@@ -0,0 +1,25 @@
+package demo.reactivestreams.part5;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class FileRunner {
+
+    public static void main(String[] args) throws InterruptedException, IOException {
+        Path file = Paths.get(System.getProperty("user.home") + "/example.txt");
+
+        TimeUnit.SECONDS.sleep(10);
+        Files.createFile(file);
+
+        TimeUnit.SECONDS.sleep(10);
+        Files.write(file, List.of("text"), StandardCharsets.UTF_8);
+
+        TimeUnit.SECONDS.sleep(10);
+        Files.delete(file);
+    }
+}
